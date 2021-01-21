45722ad04f18b1ea03bf702b047512839da06916
Ben <pauleybj@gmail.com>
2021-01-21 16:25:26 +0000

    added api functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,68 @@
-var cityArray = [];
+$(document).ready(function () {
+  var cityArray = [];
+  var lon = "";
+  var lat = "";
+  var apiKey = "39edc21fee70b725a0dbcb6dbed85366";
 
-loadStoredCities();
+  loadStoredCities();
 
-$("#add-city").on("click", function (event) {
-  event.preventDefault();
+  $("#add-city").on("click", function (event) {
+    event.preventDefault();
 
-  var city = $("#city-input").val().trim();
+    var city = $("#city-input").val().trim();
 
-  cityArray.push(city);
-  storeCityArray();
-  retrieveCityArray();
+    cityArray.push(city);
+    storeCityArray();
+    retrieveCityArray();
+  });
+
+  function storeCityArray() {
+    localStorage.setItem("cities", JSON.stringify(cityArray));
+  }
+
+  function retrieveCityArray() {
+    var retrievedData = localStorage.getItem("cities");
+    if (retrievedData !== null) {
+      cityArray = JSON.parse(retrievedData);
+
+      renderCityButtons();
+    }
+  }
+
+  function loadStoredCities() {
+    retrieveCityArray();
+  }
+
+  function renderCityButtons() {
+    $("#city-buttons-div").empty();
+    for (var i = 0; i < cityArray.length; i++) {
+      var newButton = $("<button>");
+
+      newButton.addClass("btn btn-outline-secondary city-button");
+      newButton.attr("city-name", cityArray[i]);
+      newButton.text(cityArray[i]);
+
+      $("#city-buttons-div").append(newButton);
+    }
+  }
+
+  $(document).on("click", ".city-button", getLonLat);
+
+  function getLonLat() {
+    var city = $(this).attr("city-name");
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      apiKey;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      lon = response.coord.lon;
+      lat = response.coord.lat;
+    });
+  }
+
+  function displayWeatherInfo() {}
 });
-
-function storeCityArray() {
-  localStorage.setItem("cities", JSON.stringify(cityArray));
-}
-
-function retrieveCityArray() {
-  var retrievedData = localStorage.getItem("cities");
-  if (retrievedData !== null) {
-    cityArray = JSON.parse(retrievedData);
-
-    renderCityButtons();
-  }
-}
-
-function loadStoredCities() {
-  retrieveCityArray();
-}
-
-function renderCityButtons() {
-  $("#city-buttons-div").empty();
-  for (var i = 0; i < cityArray.length; i++) {
-    var newButton = $("<button>");
-
-    newButton.addClass("btn btn-outline-secondary");
-    newButton.attr("city-name", cityArray[i]);
-    newButton.text(cityArray[i]);
-
-    $("#city-buttons-div").append(newButton);
-  }
-}
